d360ccdefbab74b058caf2d9fcdd7ace113dd639
jassem Alhasawi <alhasawj@oregonstate.edu>
2021-10-04 21:59:31 -0700

    A0

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Jasem Alhasawi
+# OSU Email: alhasawj@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: Assignment 0
+# Due Date: Sep 28 2021
+# Description: This is an acceptable version of Python, version 3.8.
+
+
+def gradescope_test():
+    return 42
